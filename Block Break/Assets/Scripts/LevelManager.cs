af38d0714e875a536fd50b36fb406ef5ff192563
mbiondi17 <mvb5bg@virginia.edu>
2017-02-08 00:26:28 -0500

    It's been a while since I pushed this...

---
+++ Block Break/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
+public class LevelManager : MonoBehaviour {
+
+    public List<string> levelNames;
+
+    public void loadScene(string sceneName)
+    {
+        if (levelNames.Contains(sceneName)) SceneManager.LoadScene(sceneName);
+        else Debug.Log("Requested a scene that does not exist or has not been added to the list of Level Names!");
+        
+    }
+
+    public void quitGame() 
+    {
+        Application.Quit();
+    }
+}
---
+++ Block Break/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+    private static MusicManager instance;
+
+	void Start () {
+
+        // set up singleton instance and ensure duplicates are removed.
+        GameObject.DontDestroyOnLoad(gameObject);
+        if (instance) GameObject.Destroy(this.gameObject);
+        else instance = this;
+	}
+	
+	void Update () {
+	
+	}
+}
