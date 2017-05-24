eb3bf4eac7a995c1bc7d2940526c90b0ed80fc58
mbiondi17 <mvb5bg@virginia.edu>
2017-05-24 00:16:23 -0400

    I don't *think* this will break anything. Folder structure had been changed in the file explorer.

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+    private static MusicManager instance;
+
+	void Awake () {
+        // set up singleton instance and ensure duplicates are removed.
+        GameObject.DontDestroyOnLoad(gameObject);
+        if (instance) GameObject.Destroy(this.gameObject);
+        else instance = this;
+	}
+	
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+    
+    private Vector3 paddlePosition;
+
+	// Use this for initialization
+	void Start () {
+        Vector3 paddlePosition = new Vector3(this.transform.position.x, this.transform.position.y, 0.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        paddlePosition.x = (Input.mousePosition.x / Screen.width) * 16;
+
+        this.transform.position = paddlePosition;
+
+	}
+}
